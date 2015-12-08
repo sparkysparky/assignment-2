9516818219e3958ce2f413cb0feae2307e8b1b26
sparkysparky <sparky274@hotmail.co.uk>
2015-12-08 16:37:22 +0000

    bbc and guardian working on top tab, rss feeds sent into url picker of php file

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,25 +5,55 @@
 		{var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id))
 		{js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}
 		(document,"script","twitter-wjs");
-
-	  /*
-		var news_item_number = 1;
-      	console.log(rss_feed);
-        $('#news-content').append('<div class ="col-sm-9 offset-3 text-left" id= "description_div' + news_item_number + '"');
-	  */
-		
 		
 	// rss feed for bbc news -->	
 	  $(document).ready(function() 
       {
+        $("#guardiantwitter").hide();
         $.ajax({
           type: "GET",
-          url: "scripts/rss_bbc.php",
+          url: "scripts/rss_picker.php",
+		  data:  {item : 0},
           dataType: "xml",
           cache: false,
           success: parse_rss_bbc
         });
     });
+	
+	//function ajax
+
+	$(document).on( 'shown.bs.tab', 'a[href="News-content1"]', function(e)
+		{
+			$("#bbctwitter").show();
+			$("#guardiantwitter").hide();
+			var newsitem = 0;
+			$.ajax({
+			type: "GET",
+			url: "scripts/rss_picker.php",
+			data: {item : newsitem},
+			dataType: "xml",
+			cache: false,
+			success: parse_rss_bbc
+        });			
+        });
+
+	$(document).on( 'shown.bs.tab', 'a[href="News-content2"]', function(e)
+		{
+			$("#bbctwitter").hide();
+			$("#guardiantwitter").show();
+			var newsitem = 6;
+			$.ajax({
+			type: "GET",
+			url: "scripts/rss_picker.php",
+			data: {item : newsitem},
+			dataType: "xml",
+			cache: false,
+			success: parse_rss_guardian
+			
+        });			
+        });
+
+	
       function parse_rss_bbc(rss_feed)
       {
       	console.log(rss_feed);
@@ -39,15 +69,16 @@
 		});
 	   }
 	   
-	   /*
-	   $('#news-content').append('<div class="col-md-12"><div class="news-image"> ' +
-			'<img src= "' + $(this).find("thumbnail").next().attr("url") + '"></div>' 
+	  function parse_rss_guardian(rss_feed)
+      {
+      	console.log(rss_feed);
+		$('#news-content').empty();
+		$(rss_feed).find("item").each(function()
+		{	
+			$('#news-content').append('<div class="col-md-12">' 
 			+ '<div class = "news-text"><p class = "title_large">' + $(this).find('title').text() + '</p>'
-			+ '<p>' + $(this).find('description').text() + '<br>' + $(this).find('pubDate').text() + ' </p>'
-			+ '<p>' + '<a href="'+  $(this).find('guid').text() + '">' + $(this).find('guid').text() +' </p>'
+			+ $(this).find('description').text() + '<br>' + $(this).find('pubDate').text() 
 			+ '</div></div>'
-			); 	
-			*/
-	   
-	   //tabs functionality
-	   
+			); 		
+		});
+	   }
