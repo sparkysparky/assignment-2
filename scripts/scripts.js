b7e1e00ff29d93052d1a001b218071e0e2d8d9cb
sparkysparky <sparky274@hotmail.co.uk>
2015-12-08 11:44:19 +0000

    Revert "rss feed for bbc working and tabs changing colour on click"
    This reverts commit 5798c3a691ad53b05e07c480ef85454150d5796a.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -13,7 +13,6 @@
 	  */
 		
 		
-		
 	// rss feed for bbc news -->	
 	  $(document).ready(function() 
       {
@@ -29,21 +28,35 @@
       function parse_rss(rss_feed)
       {
       	console.log(rss_feed);
-		$(rss_feed).find("item").each(function()
-		{	
-			$('#news-content').append('<div class="col-md-12"><div class="news-image"> ' +
-			'<img src= "' + $(this).find("thumbnail").next().attr("url") + '"></div>' 
-			+ '<div class = "news-text"><p class = "title_large">' + $(this).find('title').text() + '</p>'
-			+ '<p>' + $(this).find('description').text() + '<br>' + $(this).find('pubDate').text() + ' </p>'
-			+ '<p>' + '<a href="'+  $(this).find('guid').text() + '">' + $(this).find('guid').text() +' </p>'
-			+ '</div></div>'
-			); 
-		});
-	   }
-	   
-	   //tabs functionality
-	   
-	   
-	   
-		
-   
+        $('#news-content').append('<select>');
+        $(rss_feed).find("item").each(function()
+        {
+          $('select').append('<option value="' + 
+          $(this).find('title').text() + '">' + 
+          $(this).find('title').text() + '</option>');
+        });
+        line_record(0,rss_feed);
+        $("select").on("change", function(evt) 
+        {
+          line_record( $("select option:selected").index(),rss_feed)
+        });   
+      } 
+
+     function line_record(sel_index,rss_feed) 
+     {
+       var local_index = sel_index;
+       var image_url;                                               
+       var item_title;
+       var item_description;
+       var item_pubDate;
+       image_url = $(rss_feed).find("item").eq(local_index).find("thumbnail").last().attr("url");
+       item_title = $(rss_feed).find("item").eq(local_index).find("title").text();                               
+       item_description = $(rss_feed).find("item").eq(local_index).find("description").text();
+       item_pubDate = $(rss_feed).find("item").eq(local_index).find("pubDate").text();
+       $("#image_div").empty();
+       $("#text_div").empty();
+       $("#image_div").append("<img src='" + image_url + "'>");
+       $("#text_div").append("<span class='title_large'>" + item_title + "</span>");
+       $("#text_div").append("<p>" + item_description + "</p>");
+       $("#text_div").append("<p>" + item_pubDate + "</p>");
+     }  
