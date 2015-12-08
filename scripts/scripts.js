7ea8ccc1755496995a39e02145aa785eea99b6e0
sparkysparky <sparky274@hotmail.co.uk>
2015-12-08 11:45:58 +0000

    Revert "Revert "finished for tonight""
    This reverts commit d1599553a18d6663fbbed55e3705a0f10b869bbb.
    
    # Conflicts:
    #	scripts/scripts.js
    #	test_assignment2.html

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -28,6 +28,7 @@
       function parse_rss(rss_feed)
       {
       	console.log(rss_feed);
+<<<<<<< HEAD
         $('#news-content').append('<select>');
         $(rss_feed).find("item").each(function()
         {
@@ -60,3 +61,24 @@
        $("#text_div").append("<p>" + item_description + "</p>");
        $("#text_div").append("<p>" + item_pubDate + "</p>");
      }  
+=======
+		
+		$(rss_feed).find("item").each(function()
+		{	
+			$('#news-content').append('<div class="col-md-12"><div class="news-image"> ' +
+			'<img src= "' + $(this).find("thumbnail").next().attr("url") + '"></div>' 
+			+ '<div class = "news-text"><p class = "title_large">' + $(this).find('title').text() + '</p>'
+			+ '<p>' + $(this).find('description').text() + '<br>' + $(this).find('pubDate').text() + ' </p>'
+			+ '<p>' + '<a href="'+  $(this).find('guid').text() + '">' + $(this).find('guid').text() +' </p>'
+			+ '</div></div>'
+			); 
+		});
+	   }
+	   
+	   //tabs functionality
+	   
+	   
+	   
+		
+   
+>>>>>>> parent of d159955... Revert "finished for tonight"
